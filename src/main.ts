57ea3c29ce84a4788cc4427081af56b84fb52cb5
ljxyaly <1126925449@qq.com>
2024-03-04 18:20:39 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  });
+  app.setGlobalPrefix('api');
+  app.enableVersioning();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  // const config = new DocumentBuilder()
+  //   .setTitle('api文档')
+  //   .setDescription('api文档')
+  //   .setVersion('1.0')
+  //   .addTag('api')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, Length } from 'class-validator';
+
+// create-user.dto.ts
+export class CreateUserDto {
+  // @IsNotEmpty()
+  name: string;
+
+  // @Length(10, 20, { message: '密码长度必须在10到20之间' })
+  sex: string;
+
+  createTime: Date;
+
+  updateTime: Date;
+}
