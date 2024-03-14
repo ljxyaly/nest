1d17ff567302f23e4882beeb55c09700141bdbc2
ljxyaly <1126925449@qq.com>
2024-03-14 14:26:01 +0800

    fix: prisma调试

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
+// 重写BigInt的toJSON方法，解决Do not know how to serialize a BigInt问题
+declare global {
+  interface BigInt {
+    toJSON(): string
+  }
+}
+BigInt.prototype.toJSON = function () {
+  const int = Number.parseInt(this.toString())
+  return int ?? this.toString()
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -15,14 +26,9 @@ async function bootstrap() {
     })
   )
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('api文档')
-  //   .setDescription('api文档')
-  //   .setVersion('1.0')
-  //   .addTag('api')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder().setTitle('api文档').setDescription('api文档').setVersion('1.0').addTag('api').build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
   await app.listen(3000)
 }
 bootstrap()
