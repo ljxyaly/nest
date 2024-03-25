f248f4d3ff4a72be9d9a691bdea6e4e0536c0478
ljxyaly <1126925449@qq.com>
2024-03-25 18:33:52 +0800

    fix: 尝试迁移sequelize

--- src/module/user/user.module.ts
+++ src/module/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserService } from './user.service'
 import { UserDao } from './user.dao'
 import { UserController } from './user.controller'
 import { AuthModule } from '@/module/auth/auth.module'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { UserModel } from './user.model'
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), SequelizeModule.forFeature([UserModel])],
   providers: [UserService, UserDao],
   controllers: [UserController],
   exports: [UserService]
